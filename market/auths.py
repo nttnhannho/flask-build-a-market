e40c7eb02e21ee1dc9db721314eb96cdad2aa792
nttnhannho <makelifeperfect@gmail.com>
2022-01-11 11:54:54 +0700

    Handle form resubmission

--- market/auths.py
+++ market/auths.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from market import db
 from market.forms import RegisterForm, LoginForm
 from market.models import User
@@ -11,31 +11,44 @@ auths = Blueprint("auths", __name__)
 @auths.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
-    if form.validate_on_submit():
-        user_to_create = User(username=form.username.data, email_address=form.email_address.data, password=form.password.data)
-        db.session.add(user_to_create)
-        db.session.commit()
-        login_user(user_to_create)
-        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category="success")
-        return redirect(url_for("markets.market"))
-    if form.errors:
-        for err_msg in form.errors.values():
-            flash(f"{err_msg}", category="danger")
-    return render_template("register.html", form=form)
+
+    if request.method == "GET":
+        return render_template("register.html", form=form)
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            user_to_create = User(username=form.username.data, email_address=form.email_address.data, password=form.password.data)
+            db.session.add(user_to_create)
+            db.session.commit()
+            login_user(user_to_create)
+            flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category="success")
+            return redirect(url_for("markets.market"))
+        if form.errors:
+            for err_msg in form.errors.values():
+                flash(f"{err_msg}", category="danger")
+        return redirect(url_for("auths.register"))
 
 
 @auths.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        attempted_user = User.query.filter_by(username=form.username.data).first()
-        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
-            login_user(attempted_user)
-            flash(f"Success! You are logged in as {attempted_user.username}", category="success")
-            return redirect(url_for("markets.market"))
-        else:
-            flash(f"Incorrect user name or password! Please try again.", category="danger")
-    return render_template("login.html", form=form)
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            attempted_user = User.query.filter_by(username=form.username.data).first()
+            if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+                login_user(attempted_user)
+                flash(f"Success! You are logged in as {attempted_user.username}", category="success")
+                return redirect(url_for("markets.market"))
+            else:
+                flash(f"Incorrect user name or password! Please try again.", category="danger")
+        if form.errors:
+            for err_msg in form.errors.values():
+                flash(f"{err_msg}", category="danger")
+        return redirect(url_for("auths.login"))
+
+    if request.method == "GET":
+        return render_template("login.html", form=form)
 
 
 @auths.route("/logout")
